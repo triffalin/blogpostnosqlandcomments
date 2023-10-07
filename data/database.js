f8c2fc29b9d75ab39553d49854d8d965505c5ec2
Alin Triff <triffalin@gmail.com>
2023-10-07 18:40:46 +0300

    fixed mongoclient

--- data/database.js
+++ data/database.js
@@ -5,18 +5,18 @@ const MongoClient = mongodb.MongoClient;
 let database;
 
 async function connect() {
-  const client = await MongoClient.connect('mongodb://localhost:27017');
-  database = client.db('blog');
+	const client = await MongoClient.connect('mongodb://127.0.0.1:27017');
+	database = client.db('blog');
 }
 
 function getDb() {
-  if (!database) {
-    throw { message: 'Database connection not established!' };
-  }
-  return database;
+	if (!database) {
+		throw { message: 'Database connection not established!' };
+	}
+	return database;
 }
 
 module.exports = {
-  connectToDatabase: connect,
-  getDb: getDb
+	connectToDatabase: connect,
+	getDb: getDb,
 };
