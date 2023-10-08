65acb1d5a875c8eb001c2cce801bff3c4b621b7c
Alin Triff <triffalin@gmail.com>
2023-10-08 16:41:17 +0300

    prepared the post request data

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,5 +1,8 @@
 const loadCommentsBtnElement = document.getElementById('load-comments-btn');
 const commentsSectionElement = document.getElementById('comments');
+const commentsFormElement = document.getElementById('comments-form form');
+const commentTitleElement = document.getElementById('title');
+const commentTextElement = document.getElementById('text');
 
 function createCommentsList(comments) {
 	const commentListElement = document.createElement('ol');
@@ -28,4 +31,12 @@ async function fetchCommentsForPost() {
 	commentsSectionElement.appendChild(commentsListElement);
 }
 
+function saveComment(event) {
+	event.preventDefault();
+
+	const enteredTitle = commentTitleElement.value;
+	const enteredText = commentTextElement.value;
+}
+
 loadCommentsBtnElement.addEventListener('click', fetchCommentsForPost);
+commentsFormElement.addEventListener('submit', saveComment);
